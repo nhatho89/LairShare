3f7cf9ed1da5e96f62fea3f27cc9d078d401e436
Nhat <nhat.ho1989@gmail.com>
2016-04-20 01:22:39 -0700

    finished-footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var FooterMessage = require('./footerMessage');
 
 var Footer = React.createClass({
 
@@ -7,7 +8,8 @@ var Footer = React.createClass({
       <section className="bottom-container">
         <div className="wrap">
           <div className="footer-container">
-            <div className="footer-column">
+
+            <div className="footer-column logo-column">
               <div className="footer-logo">
                 <a className="footer-logo-img" href="/#"><img src="/assets/lairshare_logo.png" width="50" height="50" alt="lairshare"/></a>
               </div>
@@ -15,146 +17,59 @@ var Footer = React.createClass({
                 <p>© LairShare Inc. 2016</p>
               </div>
             </div>
-            <div className="footer-column">
-              <strong className="heading">Learn More</strong>
-              <ul>
-                <li><a>Features</a></li>
-      					<li><a>Integrations</a></li>
-      					<li><a>Customers</a></li>
-      					<li><a>Pricing &amp; Plans</a></li>
-              </ul>
+
+            <div className="row">
+              <div className="footer-column">
+                <strong className="heading">Learn More</strong>
+                <ul className="footer-detail">
+                  <li><a>Features</a></li>
+                  <li><a>Integrations</a></li>
+                  <li><a>Customers</a></li>
+                  <li><a>Pricing &amp; Plans</a></li>
+                </ul>
+              </div>
+
+              <div className="footer-column">
+                <strong className="heading">About</strong>
+                <ul className="footer-detail">
+                  <li><a>Company</a></li>
+                  <li><a>Jobs</a></li>
+                  <li><a>Blog</a></li>
+                </ul>
+              </div>
+
+              <div className="footer-column">
+                <strong className="heading">Support</strong>
+                <ul className="footer-detail">
+                  <li><a>Help</a></li>
+                  <li><a>Docs</a></li>
+                  <li><a>Status</a></li>
+                  <li><a>Terms</a></li>
+                </ul>
+              </div>
+
             </div>
-            <div className="footer-column">
-              <strong className="heading">About</strong>
-              <ul>
-      					<li><a>Company</a></li>
-      					<li><a>Jobs</a></li>
-      					<li><a>Blog</a></li>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <strong className="heading">Support</strong>
-              <ul>
-                <li><a>Help</a></li>
-      					<li><a>Docs</a></li>
-      					<li><a>Status</a></li>
-      					<li><a>Terms</a></li>
-              </ul>
-            </div>
-            </div>
+
             <div className="footer-column">
               <strong className="heading">CONNECT</strong>
               <div className="social">
-                <ul>
-                  <li><a href="mailto:nhat.ho1989@gmail.com"><i className="fa fa-envelope fa-2x" aria-hidden="true"></i></a></li>
-                  <li className="linkedin"><a className="icon-linkedin" href="https://www.linkedin.com/in/nhatho89"><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></li>
-                  <li className="home"><a className="icon-home" href="https://github.com/nhatho89/PersonalSite"><i className="fa fa-home fa-2x" aria-hidden="true"></i></a></li>
-                  <li className="git"><a className="icon-github-circled" href="https://github.com/nhatho89"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
-                  <li className="keep">
-
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+                <ul className="connect-row">
+                  <li><a href="mailto:nhat.ho1989@gmail.com"><i className="fa fa-envelope fa-lg" aria-hidden="true"></i></a></li>
+                  <li className="linkedin"><a className="icon-linkedin" href="https://www.linkedin.com/in/nhatho89"><i className="fa fa-linkedin fa-lg" aria-hidden="true"></i></a></li>
+                  <li className="home"><a className="icon-home" href="https://github.com/nhatho89/PersonalSite"><i className="fa fa-home fa-lg" aria-hidden="true"></i></a></li>
+                  <li className="git"><a className="icon-github-circled" href="https://github.com/nhatho89"><i className="fa fa-github fa-lg" aria-hidden="true"></i></a></li>
+                  <li className="keep"></li>
+                </ul>
               </div>
-          </section>
+            </div>
+            </div>
+
+          </div>
+          <FooterMessage/>
+      </section>
+
     );
   }
 });
 
 module.exports = Footer;
-
-// <section class="bottom-container">
-// 	<div class="wrap">
-// 		<div class="footer-container">
-// 			<div class="footer-column">
-// 				<div class="footer-logo"><a href="/"><img src="//website.smooch.io/static_assets/images/header/svg/logo.svg" width="98" height="20" alt="smooch"></a></div>
-// 				<div class="copyright">
-// 					<p>© Smooch Technologies Inc. 2015</p>
-// 				</div>
-// 			</div>
-// 			<div class="footer-column">
-// 				<strong class="heading">Learn More</strong>
-// 				<ul>
-// 					<li><a href="/is">Features</a></li>
-// 					<li><a href="/integrations">Integrations</a></li>
-// 					<li><a href="/customers">Customers</a></li>
-// 					<li><a href="/pricing">Pricing &amp; Plans</a></li>
-// 				</ul>
-// 			</div>
-// 			<div class="footer-column">
-// 				<strong class="heading">About</strong>
-// 				<ul>
-// 					<li><a href="/about">Company</a></li>
-// 					<li><a href="https://smooch.breezy.hr/#positions">Jobs</a></li>
-// 					<li><a href="http://blog.smooch.io">Blog</a></li>
-// 				</ul>
-// 			</div>
-// 			<div class="footer-column">
-// 				<strong class="heading">Support</strong>
-// 				<ul>
-//
-// 				</ul>
-// 			</div>
-// 			<div class="footer-column">
-// 				<strong class="heading">In-depth</strong>
-// 				<ul>
-// 					<li><a href="/whispers">Automated Messages</a></li>
-// 					<li><a href="/ratings">App Rating Prompt</a></li>
-// 				</ul>
-// 			</div>
-// 			<div class="footer-column">
-// 				<strong class="heading">Keep In touch</strong>
-// 				<ul>
-// 					<li><a href="mailto:hello@smooch.io">Contact</a></li>
-// 				</ul>
-// 				<div class="social">
-// 					<ul>
-// 						<li class="twitter"><a class="icon-twitter" href="https://twitter.com/SmoochLabs"></a></li>
-// 						<li class="facebook"><a class="icon-facebook-official" href="https://facebook.com/SmoochLabs"></a></li>
-// 						<li class="git"><a class="icon-github-circled" href="https://github.com/smooch"></a></li>
-// 						<li class="keep">
-// 							<a href="https://stackoverflow.com/questions/tagged/smooch+or+supportkit">
-// 								<!-- picturefill: full manual control -->
-// 								<picture>
-// 									<!--[if IE 9]><video style="display: none;"><![endif]-->
-// 									<source srcset="//website.smooch.io/static_assets/images/footer/icon-keep.png, //website.smooch.io/static_assets/images/footer/retina/icon-keep@2x.png 2x">
-// 									<!--[if IE 9]></video><![endif]-->
-// 									<img src="//website.smooch.io/static_assets/images/footer/icon-keep.png" width="15" height="20" alt="keep">
-// 								</picture>
-// 							</a>
-// 						</li>
-// 					</ul>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	</div>
-// </section>
-
-
-
-
-
-// <div className="footer-container">
-//   <div className="row-container">
-//
-//     <div>
-//       <a className="logo" href="https://github.com/nhatho89">
-//         GitHub
-//       </a>
-//     </div>
-//
-//     <div>
-//       <a className="logo" href="https://github.com/nhatho89/LairShare">
-//         Project Repo
-//       </a>
-//     </div>
-//
-//     <div>
-//       <a className="logo" href="https://www.linkedin.com/in/nhatho89">
-//         LinkedIn
-//       </a>
-//     </div>
-//
-//   </div>
-// </div>
---
+++ frontend/components/footerMessage.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var FooterMessage = React.createClass({
+
+  render: function() {
+    return (
+      <div id="footer" className="footer">
+        <div className="wrap">
+            <div className="footer-text">
+                <p><span className="mobi-hide">CAREFULLY CRAFTED by Nhat Ho for villains across the world <i className="fa fa-heart"></i></span> LET’S SPREAD CHAOS TOGETHER</p>
+            </div>
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = FooterMessage;
