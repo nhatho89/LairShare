ee7adeb7bd37262a9c53ea6960abdb58fa6539e4
Nhat <nhat.ho1989@gmail.com>
2016-04-20 19:51:21 -0700

    cleaned-up-codebase

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -18,7 +18,7 @@ var Footer = React.createClass({
               </div>
             </div>
 
-            <div className="row">
+            <div className="footer-row">
               <div className="footer-column">
                 <strong className="heading">Learn More</strong>
                 <ul className="footer-detail">
@@ -56,7 +56,7 @@ var Footer = React.createClass({
                 <ul className="connect-row">
                   <li><a href="mailto:nhat.ho1989@gmail.com"><i className="fa fa-envelope fa-lg" aria-hidden="true"></i></a></li>
                   <li className="linkedin"><a className="icon-linkedin" href="https://www.linkedin.com/in/nhatho89"><i className="fa fa-linkedin fa-lg" aria-hidden="true"></i></a></li>
-                  <li className="home"><a className="icon-home" href="https://github.com/nhatho89/PersonalSite"><i className="fa fa-home fa-lg" aria-hidden="true"></i></a></li>
+                  <li className="home"><a className="icon-home" href="http://nhatho89.github.io/PersonalSite/"><i className="fa fa-home fa-lg" aria-hidden="true"></i></a></li>
                   <li className="git"><a className="icon-github-circled" href="https://github.com/nhatho89"><i className="fa fa-github fa-lg" aria-hidden="true"></i></a></li>
                   <li className="keep"></li>
                 </ul>
