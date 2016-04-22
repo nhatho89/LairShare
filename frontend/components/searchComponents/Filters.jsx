780e8d8db4167f701a36c8f84e6def5d5acd915a
Nhat <nhat.ho1989@gmail.com>
2016-04-22 02:10:26 -0700

    minor-tweaks

--- frontend/components/searchComponents/Filters.jsx
+++ frontend/components/searchComponents/Filters.jsx
@@ -3,11 +3,12 @@ var RoomType = require('./filterComponents/room_type.jsx');
 var StayDates = require('./filterComponents/dates.jsx');
 var PriceSlider = require('./filterComponents/price_slider.jsx');
 var Guests = require('./filterComponents/guest_num.jsx');
-var Bounds = require('./filterComponents/bounds.jsx');
+// var Bounds = require('./filterComponents/bounds.jsx');
 
 var Filters = React.createClass({
 
   render: function(){
+    // <Bounds/>
     return (
       <div className="search-filters">
 
@@ -15,7 +16,6 @@ var Filters = React.createClass({
           <StayDates/>
         <hr/>
           <Guests/>
-          <Bounds/>
         <hr/>
           <PriceSlider/>
         <hr/>
